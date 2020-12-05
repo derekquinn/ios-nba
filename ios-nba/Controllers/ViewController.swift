154524579192e50c8438c5f34e5eb0b064df531a
derek quinn <quinnderek@yahoo.com>
2020-12-04 19:01:40 -0500

    Added logic to build first and last name strings from playerbaseresponse

--- ios-nba/Controllers/ViewController.swift
+++ ios-nba/Controllers/ViewController.swift
@@ -2,16 +2,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var playerTable: UITableView! // These UI elements are always force unwrapped by Apple (accepted)
+    var playersTableData: PlayerBaseResponse!
+    var playerFirstLastNames: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         NBAPlayerService.getPlayers(parameters: "", completion:{ playerBaseResponse in
             
-            print(playerBaseResponse.players[0].first_name)
+            //print(playerBaseResponse.players[0].first_name)
             
+            self.playersTableData = playerBaseResponse
+            
+            // Exposing downsides of old school MVC below (threading, if /let)
+            DispatchQueue.main.async { [self] in
+                
+                for player in self.playersTableData.players {
+                    
+                    if let firstName = player.first_name, let lastName = player.last_name {
+                        self.playerFirstLastNames.append("\(firstName) \(lastName)")
+                    }
+                    
+                    print("first name=",player.first_name,"last name=", player.last_name, playerFirstLastNames.count)
+                }
+            }
         })
     }
-
-
 }
 
